b212cdbfbc5a397ff3ebabb45f001418249a99fb
Alessandro <alessj20@gmail.com>
2022-10-22 15:02:00 -0600

    MOD: correcciones

--- validators/clients.js
+++ validators/clients.js
@@ -3,7 +3,7 @@ const validateResults = require('../utils/handleValidator');
 
 const validatorCreateItem = [
     check('name').exists().notEmpty(),
-    check('birthday').exists().notEmpty().isDate({delimiters: ['/'], format: 'YYYY/MM/DD', strictMode: false}),
+    check('birthday').exists().notEmpty(),
     check('gender').exists().notEmpty().isLength({ min: 1, max: 1 }),
     check('dpi').exists().notEmpty().isLength({ min: 13, max: 13 }),
     check('address').exists().notEmpty(),
